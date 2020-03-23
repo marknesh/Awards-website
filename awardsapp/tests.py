ac5e98ebc9694ac3f5939f19610681dd64294536
marknesh <markmunene.nesh@gmail.com>
2020-03-23 18:34:09 +0300

    add tests of profile model

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -1,14 +1,24 @@
 from django.test import TestCase
-from .models import Profile
+from .models import Profile,Projects
 
 
-class ImageTest(TestCase):
+class ProfileTest(TestCase):
 
     def setUp(self):
-        self.profile=Profile(profile_photo="fff",bio="VV",username="DD")
+        self.profile=Profile(profile_photo="dog.url",bio="awsome",username="nesh")
 
 
     def test_instance(self):
         self.assertTrue(isinstance(self.profile,Profile))
+
+
+class ProjectTest(TestCase):
+
+    def setUp(self):
+        self.project=Projects(project_name="github",project_photo="github.url",project_description="awsome stuff",project_link="github.com",pub_date="2020-09-23")
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project,Projects))
 
 # Create your tests here.
